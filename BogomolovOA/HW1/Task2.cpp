7f6cf098792fa074e4af9fa7c6d28c0921dcb262
mac <smmac072@gmail.com>
2023-10-06 18:59:03 +0300

    HW1 fix

--- BogomolovOA/HW1/Task2.cpp
+++ BogomolovOA/HW1/Task2.cpp
@@ -9,19 +9,25 @@ int main()
     // проверка на ввод нечислового числа
     bool faultyflag = false; // флаг неправильного ввода
     for (char& c : inputstring)
+    {
         if (c < 48 || c > 57) // проверка на нечисло тут
+        {
             faultyflag = true;
+        }
+    }
     if (faultyflag)
     {
-        std::cout << "haha very funny string in a number input";
-        return 1; // за такие вводы можно сразу выдавать ошибку
+        std::cout << "Not a number";
+        return 1;
     }
     long long input = stoll(inputstring); // перевод в ll
     
     // временная переменная для хранения первого символа модуля числа
     long long tmp = input;
+    
     if (tmp < 0)
         tmp *= -1;
+
     while (tmp >= 10)
         tmp /= 10;
     

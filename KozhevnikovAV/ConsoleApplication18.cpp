19713a90c9dbe5a4205a858ff6f2e6e31df1b98b
Alex Kozh <54778699+gh272b@users.noreply.github.com>
2023-10-07 14:18:55 +0300

    Created prog

---
+++ KozhevnikovAV/ConsoleApplication18.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <math.h>
+int Fibonachi(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    
+    return (Fibonachi(n - 1) + Fibonachi(n - 2));
+}
+
+int main()
+{
+    std::cout << "Task 1 - Fibonachi:\n\n";
+    
+    int n;
+    std::cout << "Write your number: ";
+    std::cin >> n;
+    std::cout << "\nAnswer: " << Fibonachi(n) << "\n\n";
+
+
+    std::cout << "Task 2 - reveresed num:\n\n";
+    int num;
+    int revNum = 0;
+    std::cout << "Write your number: ";
+    std::cin >> num;
+    
+    while (num != 0)
+    {
+        revNum *= 10;
+        revNum += num % 10;
+        num /= 10;
+    }
+    
+    std::cout << "\nAnswer: " << revNum;
+
+}
+

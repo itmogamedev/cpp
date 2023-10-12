aabb8787b4cebab4b1da5f65ec4a746be6a706d9
Georgy Titov <unknown@mail.com>
2023-10-13 00:27:26 +0300

    HomeWork Practice 1

---
+++ TitovGK/HW1/1.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+	//Переполнение char:
+	int s1 = 127, s2 = -128;
+	std::cout << (char) s1 << std::endl;
+	std::cout << (char) (s1 + 1) << std::endl;
+	std::cout << (char) s2 << std::endl;
+	std::cout << (char) (s2 - 1) << std::endl;
+
+	// Переполнение double:
+	double num = 143;
+	std::cout << pow(num, num) << std::endl;
+	std::cout << pow(num + 1, num + 1) << std::endl;
+
+	return 0;
+}

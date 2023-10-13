8a8f0f30b4822ea789e26ce613e7bddb58c0d8c4
AleksandraKirichenko <aleksaaaaa17@gmail.com>
2023-10-14 01:01:30 +0300

    text

---
+++ KirichenkoAP/HW1/дз 1.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+	int c = 0;
+	std::cout << "write a number\n";
+	std::cin >> c;
+	switch (c) {
+	case 0:
+		std::cout << 'z';
+		break;
+	case 1:
+		std::cout << 'o';
+		break;
+	case 2:
+	case 3:
+		std::cout << 't';
+		break;
+	case 4:
+	case 5:
+		std::cout << 'f';
+		break;
+	case 6:
+	case 7:
+		std::cout << 's';
+		break;
+	case 8:
+		std::cout << 'e';
+		break;
+	case 9:
+		std::cout << 'n';
+		break;
+	}
+}
---
+++ KirichenkoAP/HW1/дз 1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+	char m = '1703';
+	std::cout << "m = " << m << '\n';
+	
+	double d = 4.58390502809;
+	std::cout << "d = " << d << '\n';
+}

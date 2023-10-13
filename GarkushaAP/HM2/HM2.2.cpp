fc55bacb001c5190f40ec8ee60f031d61291fdbd
Андрей Гаркуша <147882805+And1ejGarkusha@users.noreply.github.com>
2023-10-14 01:54:00 +0300

    Update HM2.2.cpp

--- GarkushaAP/HM2/HM2.2.cpp
+++ GarkushaAP/HM2/HM2.2.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	int N,p=1,k=0;
 	cin >> N;
-	while (p <= N) //узнаем кол-во разрядов числа
+	while (p <= N)
 	{
 		p = p * 10;
 		k = k + 1;
@@ -13,9 +13,9 @@ int main()
 	int new_chislo=0, i1;
 	for (int i = k; i > 0; i = i - 1)
 	{
-		i1 = N % 10;//запоминаем последнюю цифру старого числа
-		N = N / 10;//убираем её
-		new_chislo = new_chislo + i1*(pow(10,i-1));//ставим её первой в новое число
+		i1 = N % 10;
+		N = N / 10;
+		new_chislo = new_chislo + i1*(pow(10,i-1));
 	}
 	cout << new_chislo;
 	return 0;

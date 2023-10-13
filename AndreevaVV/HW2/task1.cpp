840a5a1abdfafd4668f8317d08b3501d116116db
AndreevaVV <varya20805@gmail.com>
2023-10-14 00:23:18 +0300

    HW2

---
+++ AndreevaVV/HW2/task1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main() {
+
+    int input;
+    std::cout << "input n ";
+    std::cin >> input;
+
+    int one = 0;
+    int two = 1;
+    int other;
+
+    if (input < 1) {
+        return 0;
+    }
+    if (input == 1) {
+        std::cout << "0 1";
+    }
+    if (input > 1) {
+        std::cout << "0 1 ";
+        for (int x = 0; x < input - 2; x++) {
+            other = one + two;
+            one = two;
+            two = other;
+            std::cout << " " << other << " ";
+        }
+    }
+}
---
+++ AndreevaVV/HW2/task2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+
+	int input;
+	std::cout << "input n ";
+	std::cin >> input;
+
+	while (input > 0)
+	{
+		int x = input % 10;
+		std::cout << x;
+		input = input / 10;
+	}
+}

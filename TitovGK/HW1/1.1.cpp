d26c42c08aa0eba2921dd46ede5904cf0ec3cdea
Georgy Titov <unknown@mail.com>
2023-10-14 02:37:30 +0300

    HW1 fixed

--- TitovGK/HW1/1.1.cpp
+++ TitovGK/HW1/1.1.cpp
@@ -3,10 +3,10 @@
 
 int main() {
 	//Переполнение char:
-	int s1 = 127, s2 = -128;
-	std::cout << (char) s1 << std::endl;
+	char s1 = 127, s2 = -128;
+	std::cout << s1 << std::endl;
 	std::cout << (char) (s1 + 1) << std::endl;
-	std::cout << (char) s2 << std::endl;
+	std::cout << s2 << std::endl;
 	std::cout << (char) (s2 - 1) << std::endl;
 
 	// Переполнение double:

14791fab58bafc1fad983af75f95de5317e215da
Timofey Zhuravlev <waynaylfg@gmail.com>
2023-10-13 23:30:25 +0300

    new homework

---
+++ ZhuravlevTE/HW1/zadanie1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    const char maxChar = 255;
+    std::cout << "maxChar = " << maxChar << std::endl;
+    std::cout << "maxChar = " << maxChar + 1;
+
+    return 0;
+}
---
+++ ZhuravlevTE/HW2/zadanie2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+	int n;
+	int lastn = 0;
+	int revn = 0;
+
+	std::cout << "Enter a number: ";
+	std::cin >> n;
+
+	while (n != 0) {
+		lastn = n % 10;
+		revn = revn * 10 + lastn;
+		n = n / 10;
+	}
+
+	std::cout << "New number: " << revn;
+
+	return 0;
+}

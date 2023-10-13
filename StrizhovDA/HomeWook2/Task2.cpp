78a1b1f10b5e5a1ab34de38fb8981ebb55a38763
Strizhov-Dmitry-Andreevich <4eptik98@gmail.com>
2023-10-13 23:33:09 +0300

    Homecook2.2 fix

--- StrizhovDA/HomeWook2/Task2.cpp
+++ StrizhovDA/HomeWook2/Task2.cpp
@@ -3,6 +3,10 @@
 int main() {
     int n;
     std::cin >> n;
+    if (n < 0) {
+        std::cout << -;
+    }
+    n = abs(n);
     while (n > 0) {
         std::cout << n % 10;
         n = n / 10;

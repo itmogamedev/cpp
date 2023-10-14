24c677dafe74a55f2b9b28f670346bb8b38a20de
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2023-10-14 12:20:27 +0300

    Больше отступов богу отступов
    И меньше using namespace std.

--- J3100 Бойко М/task1.cpp
+++ J3100 Бойко М/task1.cpp
@@ -1,6 +1,6 @@
 // Задание 1 (?)
 #include <iostream>
-using namespace std;
+
 int main(){
 	char character = 257;
 	std::cout << character + 1;
--- J3100 Бойко М/task2.cpp
+++ J3100 Бойко М/task2.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
-using namespace std;
+
 int main(){
 	int number;
 	std::cin >> number;
 	switch (number)
 	{
 		case 0: 
-		std::cout << 'z';
+			std::cout << 'z';
+			break;
 		case 1:
-		std::cout << 'o';
-		break;
+			std::cout << 'o';
+			break;
 		case 2:
-		std::cout << 't';
-		break;
+			std::cout << 't';
+			break;
 		case 3:
-		std::cout << 't';
-		break;
+			std::cout << 't';
+			break;
 		case 4:
-		std::cout << 'f';
-		break;
+			std::cout << 'f';
+			break;
 		case 5:
-		std::cout << 'f';
-		break;
+			std::cout << 'f';
+			break;
 		case 6:
-		std::cout << 's';
-		break;
+			std::cout << 's';
+			break;
 		case 7:
-		std::cout << 's';
-		break;
+			std::cout << 's';
+			break;
 		case 8:
-		std::cout << 'e';
-		break;
+			std::cout << 'e';
+			break;
 		case 9:
-		std::cout << 'n';
-		break;
+			std::cout << 'n';
+			break;
 		default:
-		std::cout << "Not a single-digit number";
+			std::cout << "Not a single-digit number";
 	}
-return 0;
+	return 0;
 }

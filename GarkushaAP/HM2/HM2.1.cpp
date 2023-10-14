0edd25a4336a0b59ec455814940e5f7f9f7688f3
Андрей Гаркуша <147882805+And1ejGarkusha@users.noreply.github.com>
2023-10-15 00:15:54 +0300

    Update HM2.1.cpp

--- GarkushaAP/HM2/HM2.1.cpp
+++ GarkushaAP/HM2/HM2.1.cpp
@@ -3,17 +3,28 @@
 int main()
 {
 	int N;
-	std::cout << "Vvedite chislo, nachinaya ot 2\n";
+	std::cout << "Enter a number\n";
 	std::cin >> N;
-	std::vector<int> massiv(N);
-	massiv[0]=1, massiv[1]=1;
-	for (int i = 2; i < N; i=1+i)
-		massiv[i] = massiv[i - 1] + massiv[i - 2];
-	int i = 0;
-	while (i < N)
+	if (N <= 0)
 	{
-		std::cout << massiv[i] << " ";
-		i++;
+		std::cout << "Wrong number";
+	}
+	else if (N == 1)
+	{
+		std::cout << "1";
+	}
+	else
+	{
+		std::vector<int> array(N);
+		array[0] = 1, array[1] = 1;
+		for (int i = 2; i < N; i = 1 + i)
+			array[i] = array[i - 1] + array[i - 2];
+		int i = 0;
+		while (i < N)
+		{
+			std::cout << array[i] << " ";
+			i++;
+		}
 	}
 	return 0;
 }

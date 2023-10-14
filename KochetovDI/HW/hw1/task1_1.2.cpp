a9183636f63f86e0cdaa8aa171087466d5fa2836
deniskross <dekrosinio@gmail.com>
2023-10-15 01:52:57 +0300

    Разбиение задания 1.1. на два файла и вынесение кода из комментария.

--- KochetovDI/HW/hw1/task1_1.2.cpp
+++ KochetovDI/HW/hw1/task1_1.2.cpp
@@ -1,16 +1,8 @@
-// переполнение по типу char
-/*
-#include <iostream>
-int main() {
-    const char maxChar = 127;
-    std::cout << "maxChar = " << maxChar << std::endl;
-    std::cout << "maxChar = " << maxChar + 2;
-    return 0;
-}
 // переполнение по типу double
- */
+
 #include <iostream>
 #include <cmath>
+
 int main() {
     const double maxDouble = 1.7;
     std::cout << "maxDouble = " << maxDouble << std::endl;

8107daa6bb1cdfd30ab0ced2062c3f3c53f249d6
Андрей Гаркуша <147882805+And1ejGarkusha@users.noreply.github.com>
2023-10-14 23:43:17 +0300

    Update HM2.2.cpp

--- GarkushaAP/HM2/HM2.2.cpp
+++ GarkushaAP/HM2/HM2.2.cpp
@@ -9,13 +9,13 @@ int main()
 		p = p * 10;
 		k = k + 1;
 	}
-	int new_chislo=0, i1;
+	int newNumber=0, i1;
 	for (int i = k; i > 0; i = i - 1)
 	{
 		i1 = N % 10;
 		N = N / 10;
-		new_chislo = new_chislo + i1*(pow(10,i-1));
+		newNumber = newNumber + i1*(pow(10,i-1));
 	}
-	std::cout << new_chislo;
+	std::cout << newNumber;
 	return 0;
 }

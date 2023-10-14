118e9c1563c27b9aa3e1121c57aa6a5c12f5f7c8
Artem Blagodarenko <blagodaremkoartem32@gmail.com>
2023-10-14 03:00:58 +0300

    HM1 and HW2

---
+++ BlagodarenkoAM/HM2/HW2-fib.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, y=0, x=1,i,tmp;
+	std::cin >> a;
+	for (i = 0; i < a; i++) {
+		tmp = x + y;
+		std::cout << (tmp) << ' ';
+		y = x;
+		x = tmp;
+	}
+}
+
+
+
+

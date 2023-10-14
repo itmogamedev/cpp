a0e6080c38137926bd5f522adff055412664d82c
Alex Kozh <54778699+gh272b@users.noreply.github.com>
2023-10-14 15:44:14 +0300

    Changed logic in fib alg

--- KozhevnikovAV/ConsoleApplication18.cpp
+++ KozhevnikovAV/ConsoleApplication18.cpp
@@ -1,12 +1,9 @@
 ﻿#include <iostream>
 #include <math.h>
 int Fibonachi(int n) {
-    if (n == 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
-    }
+    if (n == 0 || n == 1) {
+        return n;
+    
     
     return (Fibonachi(n - 1) + Fibonachi(n - 2));
 }

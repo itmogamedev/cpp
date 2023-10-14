4fbfb9bad644fb60cf625127c63fe5020c375b69
Андрей Гаркуша <147882805+And1ejGarkusha@users.noreply.github.com>
2023-10-14 12:30:21 +0300

    Update HM2.1.cpp

--- GarkushaAP/HM2/HM2.1.cpp
+++ GarkushaAP/HM2/HM2.1.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 int main()
 {
 	int N;
-	cout << "Vvedite chislo, nachinaya ot 2\n";
-	cin >> N;
-	vector<int> massiv(N);
+	std::cout << "Vvedite chislo, nachinaya ot 2\n";
+	std::cin >> N;
+	std::vector<int> massiv(N);
 	massiv[0]=1, massiv[1]=1;
 	for (int i = 2; i < N; i=1+i)
 		massiv[i] = massiv[i - 1] + massiv[i - 2];
 	int i = 0;
 	while (i < N)
 	{
-		cout << massiv[i] << " ";
+		std::cout << massiv[i] << " ";
 		i++;
 	}
 	return 0;

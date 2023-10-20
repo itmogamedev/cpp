9423223fd3d00dc92c58e5393360022fb7608283
Alena Orlova <pochta@mail.ru>
2023-10-20 21:06:15 +0300

    fix_hw_1-2

--- OrlovaAA/HW2/fibonacci.cpp
+++ OrlovaAA/HW2/fibonacci.cpp
@@ -2,23 +2,28 @@
 int main() {
     int n;
     std::cin >> n;
-    if (n == 1) {
-        std::cout << 1;
-    }
-    else if (n == 2) {
-        std::cout << 1 << ' ' << 1 << ' ';
+    if (n > 0) {
+        if (n == 1) {
+            std::cout << 1;
+        }
+        else if (n == 2) {
+            std::cout << 1 << ' ' << 1 << ' ';
+        }
+        else {
+            n = n - 2;
+            int a = 1, b = 1, c;
+            std::cout << 1 << ' ' << 1 << ' ';
+            while (n > 0) {
+                c = a + b;
+                a = b;
+                b = c;
+                std::cout << b << ' ';
+                n = n - 1;
+            }
+        }
+        return 0;
     }
     else {
-        n = n - 2;
-        int a = 1, b = 1, c;
-        std::cout << 1 << ' ' << 1 << ' ';
-        while (n > 0) {
-            c = a + b;
-            a = b;
-            b = c;
-            std::cout << b << ' ';
-            n = n - 1;
-        }
+        std::cout << "error, there is no such element number in the fibonacci sequence";
     }
-    return 0;
 }
--- OrlovaAA/HW2/reverse_num.cpp
+++ OrlovaAA/HW2/reverse_num.cpp
@@ -2,10 +2,24 @@
 int main() {
     int n, a;
     std::cin >> n;
-    while (n > 0) {
-        a = n % 10;
-        std::cout << a;
-        n = n / 10;
+    if (n < 0) {
+        n = -n;
+        while (n > 0) {
+            a = n % 10;
+            std::cout << a;
+            n = n / 10;
+        }
+        std::cout << '-'; //перевернула число и добавила минус, тк отрицательное
+    }
+    else if (n >= 0 and n < 10) {
+        std::cout << n;
+    }
+    else {
+        while (n > 0) {
+            a = n % 10;
+            std::cout << a;
+            n = n / 10;
+        }
     }
 
     return 0;

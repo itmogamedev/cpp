bcbf3615229da305dd975706c97033cfa417fe72
TemirovArtem <temirovartem3@gmail.com>
2023-10-20 23:49:01 +0300

    2 commit

--- TemirovAT/HT 2/Reverse.cpp
+++ TemirovAT/HT 2/Reverse.cpp
@@ -9,7 +9,7 @@ int main()
 	string s = "";
 	std::cout << "Enter the Num to reverse: "; //  123  =>  321
 	std::cin >> N;
-	while (N > 0)
+	while (N != 0)
 	{
 		rn = rn * 10 + N % 10;
 		N = N / 10;

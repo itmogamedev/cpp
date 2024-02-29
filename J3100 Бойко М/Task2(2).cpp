f3bc7b36c0b324202da55954a4e62083e615fc5e
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2024-02-29 16:59:21 +0300

    удалил первые ДЗ из своей ветки
    Теперь их нет

--- J3100 Бойко М/Task2(2).cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-    int num;
-    std::cin >> num;
-    while(num > 0){
-        std::cout << num % 10;
-        num = num/10;
-    }
-}
--- J3100 Бойко М/task1.cpp
+++
@@ -1,9 +0,0 @@
-// Задание 1 (?)
-#include <iostream>
-
-int main(){
-	char character = 257;
-	std::cout << character + 1;
-
-	return 0;
-}

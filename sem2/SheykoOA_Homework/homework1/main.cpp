bfa94e9923d3d98beed3a4b267864fc34c0c0220
olegsheyko <surikat229official@gmail.com>
2024-03-06 21:47:02 +0300

    fix

--- sem2/SheykoOA_Homework/homework1/main.cpp
+++ sem2/SheykoOA_Homework/homework1/main.cpp
@@ -2,23 +2,15 @@
 #include <vector>
 #include <algorithm>
 
-class Lambda {
-private:
-    char beginchar;
-public:
-    Lambda(char beginchar) : beginchar(beginchar) {}
-    bool operator()(const std::string& stringdata) const {
-        return stringdata[0] == beginchar;
-    }
-};
-
 int main() {
     std::vector<std::string> strings = {"Hello", "World", "Baby", "GameDev", "Is", "Cool", "Hashbrown"};
     char symbol;
     std::cout << "Enter symbol: ", std::cin >> symbol;
-    Lambda predicate(symbol);
-    int result = std::count_if(strings.begin(), strings.end(), predicate);
+    
+    int result = std::count_if(strings.begin(), strings.end(), [symbol](const std::string& str) {
+        return str[0] == symbol;
+    });
+
     std::cout << result << std::endl;
     return 0;
 }
-

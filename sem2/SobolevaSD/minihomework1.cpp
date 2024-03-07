50bfe965ab3224a4b54f0d4244a92e302e498a3b
Sofochk <ssofochk@mail.ru>
2024-03-07 18:24:36 +0300

    Create minihomework1.cpp

---
+++ sem2/SobolevaSD/minihomework1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    vector <string> str = { "Aaaaaa", "Bbbbbb", "Gouegfow", "Obhoefi", "qegwweg", "qegw", "Ahoe" };
+    char simb;
+    cout << "Введите символ для поиска:" << endl;
+    cin >> simb;
+    int answer;
+    answer = count_if(str.begin(), str.end(), [simb](string _str) {
+        return (_str[0] == simb);
+        });
+    cout << answer;
+}
+

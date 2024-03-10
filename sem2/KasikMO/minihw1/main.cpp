382d7cbfad358ac7da8a194813aba03778a8e37e
Касик Максим Олегович <58136887+Irbis888@users.noreply.github.com>
2024-03-10 18:41:20 +0300

    Removed usindg

--- sem2/KasikMO/minihw1/main.cpp
+++ sem2/KasikMO/minihw1/main.cpp
@@ -3,15 +3,15 @@
 #include <vector>
 #include <string>
 
-using namespace std;
+
 
 int main() {
-    vector<string> v = {"Alice", "Ben", "Chloe", "Dan", "Debby", "Eve", "Farrel", "George", "Harry", "Harold", "Irene"};
+    std::vector<std::string> v = {"Alice", "Ben", "Chloe", "Dan", "Debby", "Eve", "Farrel", "George", "Harry", "Harold", "Irene"};
     auto startwithCount = [v] (char _c){
-        return count_if(v.begin(), v.end(), [_c] (string _s){ return _s[0] == _c; });
+        return std::count_if(v.begin(), v.end(), [_c] (std::string _s){ return _s[0] == _c; });
     }; //LAMBDA-EXPRESSION (itself) returns amount of strings so yes, lambda inside of lambda
-    cout << startwithCount('A') << endl; // 1
-    cout << startwithCount('D') << endl; // 2
-    cout << startwithCount('Z') << endl; // 0
+    std::cout << startwithCount('A') << std::endl; // 1
+    std::cout << startwithCount('D') << std::endl; // 2
+    std::cout << startwithCount('Z') << std::endl; // 0
 
 }

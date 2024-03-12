7ac50b957cea8e9ef842fed05de1f286429f034c
AmirovZaur <148572962+AmirovZaur@users.noreply.github.com>
2024-03-13 00:52:06 +0300

    Amirov_Zaur_HW

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    vector<string> v = { "apple", "banana", "avocado", "cherry", "apricot" };
+
+    auto starts_with_count = [](const vector<string>& vec, char ch) {
+        return count_if(vec.begin(), vec.end(), [ch](const string& s) {
+            return !s.empty() && s[0] == ch;
+            });
+        };
+
+    int count = starts_with_count(v, 'a');
+    cout << "Number of strings starting with 'a': " << count << endl;
+
+    return 0;
+}
+

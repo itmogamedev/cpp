03041f06d3cc8cbc26eb05bfdc564e3a69ecc9d8
irazaurus <ira.karaseva.andre@gmail.com>
2024-03-12 22:09:12 +0300

    Fixed russian language

--- sem2/KarasevaIA/minihomework1.cpp
+++ sem2/KarasevaIA/minihomework1.cpp
@@ -8,23 +8,23 @@ int main() {
 	int n, result;
 	std::string line;
 	char symbol;
-	std::cout << "Сколько строк в векторе? ";
+	std::cout << "How much lines? ";
 	std::cin >> n;
 
-	std::cout << "Введите строки: ";
+	std::cout << "Enter lines: ";
 	std::getline(std::cin, line);
 	for (int i = 0; i < n; i++) {
 		std::getline(std::cin, line);
 		v.push_back(line);
 	}
 
-	std::cout << "Введите символ: ";
+	std::cout << "Enter symbol: ";
 	std::cin >> symbol;
 
 	result = std::count_if(v.begin(), v.end(), [symbol](std::string s) {
 		return s.at(0) == symbol;
 		});
 
-	std::cout << "Строк, начинающихся с символа " << symbol << ", - " << result << std::endl;
+	std::cout << "Lines starting with '" << symbol << "' symbol - " << result << std::endl;
 	return 0;
 }

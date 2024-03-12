eb5a1e47755c11f790665bd07311ce0316c45699
Kochetov Denis Igorevich <dekrosinio@gmail.com>
2024-03-13 00:07:45 +0300

    Added MiniHW_1
    First commit with this HW

---
+++ sem2/KochetovDI/MiniHW/MiniHW_1/MiniHW_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main() {
+    std::vector<std::string> strings = { "Elephant", "Mountain", "Sunshine", "Butterfly", "Chocolate", "Egg"};
+
+    char startingStringChar;
+
+    auto countStringsStartingWith = [&](char ch) {
+        int count = 0;
+        for (const std::string& str : strings) {
+            if (!str.empty() && str[0] == ch) {
+                count++;
+            }
+        }
+        return count;
+        };
+
+    std::cout << "Enter the letter you want to search for:" << std::endl;
+    std::cin >> startingStringChar;
+    std::cout << "Number of strings starting with '" << startingStringChar << "': "
+        << countStringsStartingWith(startingStringChar) << std::endl;
+
+    return 0;
+}

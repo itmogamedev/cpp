07b3c3bad64e80c5257e2882e4c06dabac9f373e
AleksandraKirichenko <aleksaaaaa17@gmail.com>
2024-03-28 21:36:20 +0300

    hw1_fix + hw2

---
+++ sem2/KirichenkoAP/minihomework2/mathclient/mathclient/mathclient.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "dllmath.h"
+
+int main()
+{
+    const int size = 7;
+    int arr[size] = { 23, 45, 62, 7, 89, 11, 4};
+
+    std::cout << arithmetic_mean(arr, size) << std::endl;
+    std::cout << median(arr, size) << std::endl;
+    std::cout << quadratic_mean(arr, size) << std::endl;
+    std::cout << dispersion(arr, size) << std::endl;
+
+}
